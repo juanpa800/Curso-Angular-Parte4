72ec1646cc2a4059bdd8d4a5645b68f97be437a8
canojuanpa800 <canojuanpa@gmail.com>
2022-07-04 18:13:02 -0500

    add guestList component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GuestListComponent } from './guest-list/guest-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GuestListComponent,
   ],
   imports: [
     BrowserModule,
